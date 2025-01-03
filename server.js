1a097ca5c0e690ef27d2c4c739df7bea02e5ef9a
VARUNREDDY021 <144226567+VARUNREDDY021@users.noreply.github.com>
2025-01-03 16:06:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, AWS CI/CD Pipeline!');
+  res.send('Hello, AWS CI/CD Pipeline - Updated!');
 });
 
 app.listen(port, () => {
